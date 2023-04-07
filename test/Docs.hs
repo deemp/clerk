afcd3b28b4d699a697ced9aadfa1d6d0f8281a6f
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-04-07 17:31:47 +0300

    fix test

--- test/Docs.hs
+++ test/Docs.hs
@@ -1,34 +1,15 @@
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
-{-# HLINT ignore "Redundant bracket" #-}
-
 module Main (main) where
 
-import Converter (hsToMd)
-import Data.Default (def)
-import Data.String (IsString (..))
+import Control.Monad (forM, (>=>))
+import Converter (Format (..), convertTo, def)
+import Data.String.Interpolate (i)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
-import Turtle (MonadIO (liftIO), readTextFile, sh, writeTextFile)
 
 main :: IO ()
 main = do
-  T.putStrLn "Converting README"
-  sh $ liftIO $ do
-    intro <- readTextFile "README/Intro.md"
-    let examplesPaths = (\(x :: Int) -> fromString $ "app/Example" <> show x <> ".hs") <$> [1 .. 4]
-        convertExamples = ((T.pack . hsToMd def . T.unpack) <$>) . readTextFile <$> examplesPaths
-    examples <- sequenceA convertExamples
-    outro <- readTextFile "README/Outro.md"
-    writeTextFile "README.md" (T.intercalate "\n" ([intro] <> examples <> [outro]))
+  intro <- T.readFile "README/Intro.md"
+  let examplesPaths = (\(x :: Int) -> [i|app/Example#{x}.hs|]) <$> [1 .. 4]
+  examples <- forM examplesPaths (T.readFile >=> (pure . (Hs `convertTo` Md) def))
+  outro <- T.readFile "README/Outro.md"
+  T.writeFile "README.md" (T.intercalate "\n" ([intro] <> examples <> [outro]))
