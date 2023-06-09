2b65d4aa6695868fb7a0453013ca229a887f967d
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-09 11:49:42 +0300

    add symbol operations

---
+++ src/Clerk/Symbols.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Clerk.Symbols where
+
+import Clerk.Coordinates
+import Clerk.Internal
+import Codec.Xlsx (ColumnIndex, RowIndex)
+import Data.Char
+import Data.Data
+import Data.Default
+import qualified Data.Text as T
+import Fcf (Eval, Exp, Fst, Snd, type (++))
+import Fcf.Class.Foldable
+import Fcf.Data.List (DropWhile, Span, TakeWhile)
+import GHC.Base
+import GHC.TypeLits as TL
+
+type family FromChars1 (cs :: [Char]) (res :: Symbol) :: Symbol where
+  FromChars1 '[] res = res
+  FromChars1 (c : cs) res = FromChars1 cs (ConsSymbol c res)
+
+type family FromChars (cs :: [Char]) :: Symbol where
+  FromChars cs = FromChars1 (Reverse cs '[]) ""
+
+type family Reverse (s :: [Char]) (res :: [Char]) :: [Char] where
+  Reverse '[] res = res
+  Reverse (c : cs) res = Reverse cs (c : res)
+
+type family ToChars1 (s :: Maybe (Char, Symbol)) (r :: [Char]) :: [Char] where
+  ToChars1 Nothing s = Reverse s '[]
+  ToChars1 ('Just '(c, cs)) s = ToChars1 (UnconsSymbol cs) (c ': s)
+
+type family ToChars (s :: Symbol) :: [Char] where
+  ToChars s = ToChars1 (UnconsSymbol s) '[]
+
+type family CharBetween1 (c1 :: Ordering) (c2 :: Ordering) :: Bool where
+  CharBetween1 EQ LT = True
+  CharBetween1 LT EQ = True
+  CharBetween1 LT LT = True
+  CharBetween1 a b = False
+
+type family CharBetween (c :: Char) (lowerBound :: Char) (upperBound :: Char) :: Bool where
+  CharBetween c lowerBound upperBound = CharBetween1 (CmpChar lowerBound c) (CmpChar c upperBound)
+
+type family AssertNonEmptyList (s :: [a]) (errorMessage :: ErrorMessage) :: [a] where
+  AssertNonEmptyList '[] errorMessage = TypeError errorMessage
+  AssertNonEmptyList a errorMessage = a
