0fa6efc269ffe31cafce469583324203e61124e2
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-12 13:43:58 +0300

    fix path

--- test/Docs.hs
+++ test/Docs.hs
@@ -9,7 +9,7 @@ import Data.Text.IO as T (readFile, writeFile)
 main :: IO ()
 main = do
   intro <- T.readFile "README/Intro.md"
-  let examplesPaths = (\(x :: Int) -> [i|app/Example#{x}.hs|]) <$> [1 .. 4]
+  let examplesPaths = (\(x :: Int) -> [i|Examples/Example#{x}.hs|]) <$> [1 .. 4]
   examples <- forM examplesPaths (T.readFile >=> (pure . (Hs `convertTo` Md) def))
   outro <- T.readFile "README/Outro.md"
   T.writeFile "README.md" (T.intercalate "\n" ([intro] <> examples <> [outro]))
